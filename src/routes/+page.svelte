cef05a79b6210ad4c23ae73a9a8725e2b68b51f2
sekhmych <sekhmych@gmail.com>
2025-09-10 10:37:45 +0900

    Hot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import img_sekhmych_art from "$lib/assets/img/sekhmych_art.png";
-    import img_sekhmych_real from "$lib/assets/img/sekhmych_real.jpg";
+    import img_sekhmych_art from "$lib/assets/img/sekhmych-art.png";
+    import img_sekhmych_real from "$lib/assets/img/sekhmych-real.jpg";
     import img_android from "$lib/assets/img/android.png";
     import img_api from "$lib/assets/img/api.png";
     import img_db from "$lib/assets/img/db.png";
     import img_email from "$lib/assets/img/email.png";
-    import img_guess_app from "$lib/assets/img/guess_app.png";
-    import img_meditation_app from "$lib/assets/img/meditation_app.png";
+    import img_guess_app from "$lib/assets/img/guess-app.png";
+    import img_meditation_app from "$lib/assets/img/meditation-app.png";
     import img_skills from "$lib/assets/img/skills.png";
     import img_tg from "$lib/assets/img/tg.png";
     import img_vk from "$lib/assets/img/vk.png";
-    import img_weather_app from "$lib/assets/img/weather_app.png";
+    import img_weather_app from "$lib/assets/img/weather-app.png";
     import img_web from "$lib/assets/img/web.png";
     import img_github from "$lib/assets/img/github.png";
     import {
@@ -78,9 +78,9 @@
 
     <!--Navbar-->
     <div
-        class="flex bg-black top-0 w-full text-white h-18 items-center justify-items-center justify-center"
+        class="flex bg-black top-0 w-full text-white h-fit items-center justify-items-center justify-center"
     >
-        <nav class="space-x-4">
+        <nav class="flex flex-col lg:flex lg:flex-row lg:space-x-4 lg:h-16  items-center justify-center">
             <!--<a class="hover:border-b-2 border-white transition-all" href="#main"
                 >ГЛАВНАЯ</a
             >-->
@@ -105,43 +105,43 @@
 
     <!--Начало-->
     <div id="main" class="text-center w-full mt-12">
-        <h2 class="text-5xl mt-6 tracking-widest">ПОРТФОЛИО</h2>
+        <h2 class="text-5xl mt-6 tracking-widest transition-all delay-75 duration-300 ease-in hover:tracking-[2rem]">ПОРТФОЛИО</h2>
     </div>
 
     <div
-        class="scrollbar-hide overflow-x-hidden absolute right-[-7.5rem] top-0 -z-10 blur-lg rounded-full w-96 h-96 bg-purple-500"
+        class="scrollbar-hide overflow-x-hidden absolute right-[-7.5rem] top-0 -z-10 blur-lg rounded-full w-64 h-64 lg:w-96 lg:h-96 bg-purple-500"
     ></div>
     <!--Вступительное-->
     <div
-        class="flex items-center justify-center justify-items-center p-4 mt-12"
+        class="flex lg:flex-row flex-col items-center justify-center justify-items-center p-4 mt-12"
     >
         <img
-            class="w-64 h-64 rounded-full"
+            class="lg:w-64 lg:h-64 w-32 h-32 rounded-full transition-all delay-75 duration-300 ease-in-out hover:scale-150 hover:translate-x-8"
             title="Автор (рисунок)"
             src={img_sekhmych_art}
             alt="Автор в виде рисунка"
         />
         <h2
-            class="relative cols-3 m-auto text-5xl items-center justify-center justify-items-center text-center w-[36rem]"
+            class="relative cols-3 m-auto text-2xl lg:text-5xl items-center justify-center justify-items-center text-center lg:w-[36rem]"
         >
             <b class="text-purple-500">Сергеев</b> Владимир<br />
             {#key cur_prof}
                 <div
-                    class="absolute m-auto text-center w-full"
+                    class="absolute lg:relative m-auto text-center"
                     transition:slide
                 >
-                    <b class="text-purple-500 m-aut text-3xl">{cur_prof}</b>
+                    <b class="text-purple-500 m-auto text-2xl lg:text-3xl">{cur_prof}</b>
                 </div>
             {/key}
             <br /><a
                 href="https://www.sakha.gov.ru/"
                 target="_blank"
-                class="font-light text-[2rem] hover:border-b-2 border-purple-400 transition-all duration-75 ease-in-out"
+                class="font-light text-[1.25rem] lg:text-[2rem] hover:border-b-2 border-purple-400 transition-all duration-75 ease-in-out"
                 >&mdash;>Из Якутии</a
             >
         </h2>
         <img
-            class="w-64 h-64 rounded-full"
+            class="lg:w-64 lg:h-64 w-32 h-32 rounded-full transition-all delay-75 duration-300 ease-in-out hover:scale-150 hover:-translate-x-8"
             title="Автор (в реальности)"
             src={img_sekhmych_real}
             alt="Автор. Молодой 19-летний специалист с усами"
@@ -168,12 +168,12 @@
     <!--Мои специализации-->
     <div id="specializations" class="items-center justify-items-center mt-56">
         <h2
-            class="text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
+            class="text-4xl lg:text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
         >
             МОИ СПЕЦИАЛИЗАЦИИ
         </h2>
         <p
-            class="text-center w-[56rem] mt-8 transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default hover:bg-black hover:text-white hover:p-2"
+            class="lg:text-2xl text-center w-fit lg:w-[56rem] mt-8 transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default hover:bg-black hover:text-white hover:p-2"
         >
             Я специалист с опытом разработки веб и мобильных приложений. Я люблю
             изучать новые технологии. Мне нравится вникать в требования к
@@ -181,7 +181,7 @@
         </p>
 
         <div
-            class="grid grid-cols-2 items-center justify-center gap-x-44 gap-12 mt-16 space-x-36"
+            class="grid grid-cols-2 items-center justify-center gap-x-2 lg:gap-x-44 gap-2 lg:gap-12 mt-16 space-x-4 lg:space-x-36"
         >
             <div
                 class="grid-cols-2 m-auto items-center justify-items-center font-bold transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default"
@@ -192,7 +192,7 @@
                     src={img_android}
                     alt="Лого Андроид"
                 />
-                <h5 class="text-center">Андроид-разработчик<br />(junior)</h5>
+                <h5 class="text-center text-sm lg:text-md">Андроид-разработчик<br />(junior)</h5>
             </div>
             <div
                 class="grid-cols-2 m-auto items-center justify-items-center font-bold transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default"
@@ -203,7 +203,7 @@
                     src={img_api}
                     alt="Лого API"
                 />
-                <h5 class="text-center">
+                <h5 class="text-center text-sm lg:text-md">
                     Инженер по разработке API<br />(junior)
                 </h5>
             </div>
@@ -216,7 +216,7 @@
                     src={img_web}
                     alt="Лого веб-разработка"
                 />
-                <h5 class="text-center">Веб-разработчик<br />(junior)</h5>
+                <h5 class="text-center text-sm lg:text-md">Веб-разработчик<br />(junior)</h5>
             </div>
             <div
                 class="grid-cols-2 m-auto items-center justify-items-center font-bold transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default"
@@ -227,7 +227,7 @@
                     src={img_db}
                     alt="Лого БД"
                 />
-                <h5 class="text-center">
+                <h5 class="text-center text-sm lg:text-md">
                     Администратор баз данных<br />(junior)
                 </h5>
             </div>
@@ -240,12 +240,12 @@
     <!--Мои работы-->
     <div id="works" class="mt-24 items-center justify-items-center">
         <h2
-            class="text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
+            class="text-4xl lg:text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
         >
             МОИ РАБОТЫ
         </h2>
         <p
-            class="text-center w-[56rem] mt-8 transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default hover:bg-black hover:text-white hover:p-2"
+            class="text-center w-fit lg:w-[56rem] mt-8 transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default hover:bg-black hover:text-white hover:p-2"
         >
             Важно развивать в себе хорошую трудовую этику. Проявляйте себя во
             всем, чем бы вы ни занимались, будь то уборщик или ваша первая
@@ -254,7 +254,7 @@
         </p>
 
         <div
-            class="flex text-center justify-items-center items-center justify-center space-x-8 mt-8"
+            class="flex flex-col space-y-8 lg:space-y-0 lg:flex-row text-center justify-items-center items-center  space-x-8 mt-8"
         >
             <div
                 class="group relative transition delay-75 duration-300 ease-in-out hover:scale-110 bg-green-400 w-64 h-96 rounded-2xl"
@@ -343,30 +343,39 @@
         class="what-i-can-do mt-24 items-center justify-center justify-items-center"
     >
         <h2
-            class="text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
+            class="text-4xl lg:text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
         >
             ЧТО Я УМЕЮ
         </h2>
         <p
-            class="text-center w-[56rem] mt-8 transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default hover:bg-black hover:text-white hover:p-2"
+            class="text-center w-fit lg:w-[56rem] mt-8 transition-all delay-150 duration-300 ease-in-out hover:scale-125 cursor-default hover:bg-black hover:text-white hover:p-2"
         >
             Единственный, с кем вам следует сравнивать себя, - это вы сами. Ваша
             миссия - стать сегодня лучше, чем вы были вчера. Вы делаете это,
             сосредоточившись на том, что вы можете сделать сегодня, чтобы
             совершенствоваться и расти.
         </p>
-        <img
-            title="Мои скиллы"
-            class="w-[56rem]"
-            src={img_skills}
-            alt="Умения автора: Java; Pyhton; Kotlin; C; C++; C#; JavaScript (JS); PostMan; PyCharm; Git; VS Code; Android Studio; Visual Studio; Intellij Idea; MySql; SqLite; Firebase"
-        />
+        <div
+            class="flex flex-col lg:flex-row group relative transition-all delay-75 duration-300 ease-in-out"
+        >
+            <img
+                title="Мои скиллы"
+                class="w-[56rem] group-hover:opacity-0 transition-all delay-1000 duration-300 ease-in-out"
+                src={img_skills}
+                alt="Умения автора: Java; Pyhton; Kotlin; C; C++; C#; JavaScript (JS); PostMan; PyCharm; Git; VS Code; Android Studio; Visual Studio; Intellij Idea; MySql; SqLite; Firebase"
+            />
+            <h2
+                class="lg:absolute w-auto opacity-100 lg:opacity-0  text-center lg:top-1/2 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2 -z-10 text-2xl lg:text-4xl  transition-all delay-[1500ms] duration-300 ease-in-out group-hover:opacity-100"
+            >
+                Но самый главный скилл &mdash; <p class="text-purple-500">умение изучать новое</p>
+            </h2>
+        </div>
     </div>
 
     <!--Конец-->
     <div id="contacts" class="w-full mt-24">
         <h2
-            class="text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
+            class="text-4xl lg:text-5xl text-center transition-all delay-150 duration-300 ease-in-out hover:scale-125"
         >
             ГОТОВ К НАЙМУ
         </h2>
@@ -408,7 +417,7 @@
             >
         </div>
         <h2
-            class="text-5xl text-center mt-12 mb-24 tracking-[1.5rem] transition-all delay-150 duration-300 ease-in-out hover:scale-125"
+            class="text-4xl lg:text-5xl text-center mt-12 mb-24 tracking-[1.5rem] transition-all delay-150 duration-300 ease-in-out hover:scale-125"
         >
             НАПИШИТЕ МНЕ СЕЙЧАС
         </h2>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
