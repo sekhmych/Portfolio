964c900a79b01babb2d39c82c7173ff4ac7f1dbb
sekhmych <sekhmych@gmail.com>
2025-09-10 12:01:05 +0900

    Hot10

--- src/routes/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { error } from "@sveltejs/kit";
-
-</script>
-<div class="h-screen">
-    <h1 class="text-5xl font-light text-center">И всё опять сломалось...</h1>
-    <p>{error}</p>
-</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "svelte-adapter-bun";
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { sveltekit } from '@sveltejs/kit/vite';
 
@@ -13,8 +13,16 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
-	},
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 
 };
 
